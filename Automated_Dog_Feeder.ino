f2db8293f4ad7f1fe96e770f2cef953295329640
Rabiteson <128616092+Rabiteson@users.noreply.github.com>
2023-04-13 19:16:09 -0500

    Add files via upload

--- Automated_Dog_Feeder.ino
+++ Automated_Dog_Feeder.ino
@@ -7,12 +7,12 @@
  #include "Wire.h" // For I2C
  #include "LCD.h" // For LCD
  #include "LiquidCrystal_I2C.h" // for I2C LCD backpack
- #include <SPI.h> // for SD card
+ //#include <SPI.h> // for SD card
  #include <SD.h> // for SD card
  #include <HX711.h> // for load cell ADC
  #include <ThreeWire.h> // software SPI for RTC
  #include <RtcDS1302.h> // for RTC
- #include <TMRpcm.h> // .wav library
+ #include <TMRpcm.h>   // .wav library
  #include "PN532_I2C.h" //RFID I2C
  #include "PN532.h" //RFID
  #include "NfcAdapter.h" //RFID Read Data
@@ -20,6 +20,7 @@
  // end library Inclusions =======================================================================================================
  
  // I/O pin Designations =========================================================================================================
+ #define SD_ChipSelectPin 53
  #define SLScontrol 9 //control pin for SLS servo motor
  #define menu 3 // menu button with interrupt
  #define up 4 // up button
@@ -27,16 +28,16 @@
  #define back 6 // back button
  #define enter 7 // enter button
 
- #define SCALE_LEFT_DOUT 23 //
- #define SCALE_LEFT_SCK 25 //
- #define SCALE_RIGHT_DOUT 22 //
- #define SCALE_RIGHT_SCK 24 //
+ #define SCALE_LEFT_DOUT 23 //left load cell data
+ #define SCALE_LEFT_SCK 25 //left load cell clock
+ #define SCALE_RIGHT_DOUT 22 //right load cell data
+ #define SCALE_RIGHT_SCK 24 //right load cell clock
 
  #define RTC_CLK 47
  #define RTC_DAT 48
  #define RTC_RST 49
 
- #define SOUND 46
+ #define SOUND 46 // Audio output
 
  #define MOTOR_LEFT_OPEN 31 //green
  #define MOTOR_LEFT_CLOSE 29 // yellow
@@ -75,22 +76,33 @@
 
 //Structs===============================================================================================================================
 
+  typedef struct FakeDateTime{
+    uint16_t year;
+    uint8_t month;
+    uint8_t dayOfMonth;
+    uint8_t hour;
+    uint8_t minute;
+    uint8_t second;
+  };
+  typedef struct timeString{
+    char hours[2];
+    char mins[2];
+    
+    
+  };
+  
   typedef struct feeding{
     
     uint8_t quartercups; // byte expecting values 1-16 correspondint to the number of quarter cups
     uint8_t hour; 
-    uint8_t minutes;
-    String noise;
+    uint8_t mins;
+    char noise[9]; // needs to be a constant length char array
   };
-  feeding leftMorning; // left dog morning feeding schedule to be kept at EEPROM Add. 8
-  feeding leftEvening; // left dog evening feeding schedule to be kept at EEPROM Add. 16
-  feeding rightMorning; // right dog morning feeding schedule to be kept at EEPROM Add. 24
-  feeding rightEvening; // right dog evening feeding schedule to be kept at EEPROM Add. 32
-
- // set up variables using the SD utility library functions:
- Sd2Card card;
- SdVolume volume;
- SdFile root;
+  feeding leftMorning; // left dog morning feeding schedule to be kept at EEPROM Add. 0
+  feeding leftEvening; // left dog evening feeding schedule to be kept at EEPROM Add. 1000
+  feeding rightMorning; // right dog morning feeding schedule to be kept at EEPROM Add. 2000
+  feeding rightEvening; // right dog evening feeding schedule to be kept at EEPROM Add. 3000
+// instantiation of objects
  PN532_I2C pn532_i2c(Wire);
  NfcAdapter nfc = NfcAdapter(pn532_i2c);
 
@@ -102,8 +114,7 @@
  RtcDS1302<ThreeWire> Clock(RTC_Serial);
  TMRpcm speak; // audio output object
  
-
- // Right arrow for menu
+// Right arrow for menu
 byte ARROW[8] = {
   B00000,
   B00100,
@@ -115,11 +126,25 @@ byte ARROW[8] = {
   B00000
 };
 
+//functions ######################################################################################################################
+void displayTime(){
+  
+}
 
- //functions ######################################################################################################################
+FakeDateTime Real2FakeDate(RtcDateTime) {
+  FakeDateTime fakedate;
+    uint16_t year;
+    uint8_t month;
+    uint8_t dayOfMonth;
+    uint8_t hour;
+    uint8_t minute;
+    uint8_t second;
+  }
+
 String readNFC() {
   Serial.println("Reading NFC");
   if (nfc.tagPresent()){
+    speak.play("beep.wav");
     NfcTag tag = nfc.read();
     tag.print();
     tagId = tag.getUidString();
@@ -127,7 +152,7 @@ String readNFC() {
     Serial.println(tagId);
   }
   else {
-  return("NoDog");
+  return(F("NoDog"));
   };
 
   return tagId;
@@ -147,7 +172,7 @@ void RFID() {
       open_left();
       close_right();
     }
-  if(tagId=="NoDog"){
+  if(tagId==(F("NoDog"))){
     close_right();
     close_left();
   }
@@ -233,6 +258,34 @@ int open_left(){
   SLS_Idle();
  }
 
+void printFeeding(feeding input){
+  Serial.println("printing feed time");
+  float cups = (float(input.quartercups) * 0.25);
+  String noise = input.noise;
+  lcd.clear();
+  lcd.setCursor(0,1);
+  lcd.print(cups);
+  lcd.print("c ");
+  lcd.print(input.hour);
+  lcd.print(":");
+  lcd.print(input.mins);
+  lcd.print(" ");
+  lcd.println(input.noise);
+
+  Serial.print(cups);
+  Serial.print("c ");
+  Serial.print(input.hour);
+  Serial.print(":");
+  Serial.print(input.mins);
+  Serial.print(" ");
+  Serial.println(input.noise);
+  Serial.print("in struct:");
+  Serial.println(input.noise);
+  Serial.print("string:");
+  Serial.println(noise);
+}
+
+// User Interface #######################################################################################################################################
  void ISR_UI(){
   userInterface = true;
  }
@@ -242,6 +295,10 @@ int open_left(){
   String Left_Dog = "Left Dog";
   String Right_Dog = "Right Dog";
   String Option = "Option";
+  RtcDateTime timeset;
+  feeding feedset = {1, 10, 30, "test.wav\0"};
+  boolean isDog; // true for left dog false for right
+  boolean isMorning; // true for morning false for evening
   
    main_clock:
    lcd.clear();
@@ -258,7 +315,10 @@ int open_left(){
     if(!digitalRead(up))goto main_option;
     if(!digitalRead(down)) goto main_left;
     if(!digitalRead(back)) goto UI_exit;
-    if(!digitalRead(enter)) goto Clock_Set;
+    if(!digitalRead(enter)){
+      timeset = Clock.GetDateTime();
+      goto Clock_Set;
+    }
    }
 
    main_left:
@@ -274,10 +334,13 @@ int open_left(){
    lcd.print(Option);
    delay(buttondelay);
    while(true){
-    if(!digitalRead(up))goto main_clock;
+    if(!digitalRead(up)) goto main_clock;
     if(!digitalRead(down)) goto main_right;
     if(!digitalRead(back)) goto UI_exit;
-    //if(!digitalRead(enter)) goto
+    if(!digitalRead(enter)){
+      isDog = true;
+      goto morning;
+    }
    }
 
    main_right:
@@ -296,10 +359,120 @@ int open_left(){
     if(!digitalRead(up))goto main_left;
     if(!digitalRead(down)) goto main_option;
     if(!digitalRead(back)) goto UI_exit;
-    //if(!digitalRead(enter)) goto
+    if(!digitalRead(enter)){
+      isDog = false;
+      goto morning;
+    }
    }
 
-   main_option:
+   morning:
+   lcd.clear();
+   lcd.write(9);
+   lcd.print("Morning");
+   lcd.setCursor(1,1);
+   lcd.print("Evening");
+   delay(buttondelay);
+   while(true){
+    if(!digitalRead(up))goto evening;
+    if(!digitalRead(down)) goto evening;
+    if(!digitalRead(back)){
+      if(isDog){
+        goto main_left;
+      }
+      else goto main_right;
+    }
+    if(!digitalRead(enter)){
+      isMorning = true;
+      goto set_feeding;
+    }
+   }
+
+   evening:
+   lcd.clear();
+   lcd.setCursor(1,0);
+   lcd.print("Morning");
+   lcd.setCursor(0,1);
+   lcd.write(9);
+   lcd.print("Evening");
+   delay(buttondelay);
+   while(true){
+    if(!digitalRead(up))goto evening;
+    if(!digitalRead(down)) goto evening;
+    if(!digitalRead(back)){
+      if(isDog){
+        goto main_left;
+      }
+      else goto main_right;
+     }
+    if(!digitalRead(enter)){
+      isMorning = false;
+      goto set_feeding;
+    }
+   }
+  
+  set_feeding:
+//  lcd.clear();
+//  lcd.setCursor(0,0);
+//  lcd.print("In Set Feeding");
+  printFeeding(feedset);
+  delay(buttondelay);
+    while(true){
+    if(!digitalRead(up)){
+      if(feedset.quartercups<16){
+        feedset.quartercups++;
+      }
+       goto set_feeding;
+    }
+    if(!digitalRead(down)){
+      if(feedset.quartercups>1){
+        feedset.quartercups--;
+      }
+      goto set_feeding;
+    }
+    if(!digitalRead(back)){
+      if(isMorning)goto morning;
+      else goto evening;
+    }
+    if(!digitalRead(enter)) goto set_feeding_hrs;
+   }
+
+ set_feeding_hrs:
+   printFeeding(feedset);
+   delay(buttondelay);
+    while(true){
+    if(!digitalRead(up)){
+      if(feedset.hour<23) feedset.hour++;
+      goto set_feeding_hrs;
+    }
+    if(!digitalRead(down)){
+      if(feedset.hour>0) feedset.hour--;
+      goto set_feeding_hrs;
+    }
+    if(!digitalRead(back)) goto set_feeding;
+    if(!digitalRead(enter)) goto set_feeding_mins;
+   }
+
+   set_feeding_mins:
+   printFeeding(feedset);
+   delay(buttondelay);
+    while(true){
+    if(!digitalRead(up)){
+      if(feedset.mins<59) feedset.mins++;// = feedset.hour + 1;
+      goto set_feeding_mins;
+    }
+    if(!digitalRead(down)){
+      if(feedset.hour>0) feedset.mins--;// = feedset.hour - 1;
+      goto set_feeding_mins;
+    }
+    if(!digitalRead(back)) goto set_feeding_hrs;
+    //if(!digitalRead(enter)) goto set_feeding_mins;
+   }
+
+   
+    
+   
+
+  main_option:
    lcd.clear();
    lcd.setCursor(1,0);
    lcd.print(Set_Clock);
@@ -323,10 +496,13 @@ int open_left(){
   
   Clock_Set:
   lcd.clear();
-  lcd.print("the clock setting interface goes here");
+  //printDateTime(timeset);
   delay(buttondelay);
   while(true){
-    //if(!digitalRead(up))goto main_right;
+    if(!digitalRead(up)){
+     //timeset.Month() = timeset.Month()+1;
+     goto Clock_Set;
+    }
     //if(!digitalRead(down)) goto main_clock;
     if(!digitalRead(back)) goto main_clock;
     //if(!digitalRead(enter)) goto
@@ -347,6 +523,7 @@ int open_left(){
     if(!digitalRead(back)) goto main_option;
     if(!digitalRead(enter)){SetOption(true); goto main_option;}
    }
+   
    OptionB:
   lcd.clear();
   lcd.print("Feed simultaneously?");
@@ -360,16 +537,20 @@ int open_left(){
     if(!digitalRead(up))goto OptionA;
     if(!digitalRead(down)) goto OptionA;
     if(!digitalRead(back)) goto main_option;
-    if(!digitalRead(enter)){SetOption(false); goto main_option;}
-   }
+    if(!digitalRead(enter)){
+      SetOption(false);
+      goto main_option;
+     }
+    }
+   
   
   UI_exit:
   userInterface = false;
   lcd.clear();
   return;
- } // End User Interface**************************
+ } // End User Interface*********************************************************************************************************************************
 
- void SetOption(bool toSet){
+ void SetOption(boolean toSet){
   if(toSet=true){allowSimultaneousFeeding = true; digitalWrite(13, LOW);}
   
   else if(toSet=false){allowSimultaneousFeeding = false; digitalWrite(13, LOW);}
@@ -422,95 +603,32 @@ void setup() {
   lcd.clear();
 
   Clock.Begin();
-
+  
+  pinMode(SOUND, OUTPUT);
+  digitalWrite(SOUND, LOW);
   speak.speakerPin = SOUND;
-  speak.setVolume(7);
+  speak.volume(7);
+
+    if (!SD.begin(SD_ChipSelectPin)) {  // see if the card is present and can be initialized:
+    Serial.println("SD fail");  
+    return;   // don't do anything more if not
+  }  
 
  // Test code below here in setup ***************************************************************************************************
-  
-
-  Serial.print("\nInitializing SD card...");
-
-  // we'll use the initialization code from the utility libraries
-  // since we're just testing if the card is working!
-  if (!card.init(SPI_HALF_SPEED, chipSelect)) {
-    Serial.println("initialization failed. Things to check:");
-    Serial.println("* is a card inserted?");
-    Serial.println("* is your wiring correct?");
-    Serial.println("* did you change the chipSelect pin to match your shield or module?");
-    while (1);
-  } else {
-    Serial.println("Wiring is correct and a card is present.");
-  }
-
-  // print the type of card
-  Serial.println();
-  Serial.print("Card type:         ");
-  switch (card.type()) {
-    case SD_CARD_TYPE_SD1:
-      Serial.println("SD1");
-      break;
-    case SD_CARD_TYPE_SD2:
-      Serial.println("SD2");
-      break;
-    case SD_CARD_TYPE_SDHC:
-      Serial.println("SDHC");
-      break;
-    default:
-      Serial.println("Unknown");
-  }
-
-  // Now we will try to open the 'volume'/'partition' - it should be FAT16 or FAT32
-  if (!volume.init(card)) {
-    Serial.println("Could not find FAT16/FAT32 partition.\nMake sure you've formatted the card");
-    while (1);
-  }
-
-  Serial.print(F("Clusters:          "));
-  Serial.println(volume.clusterCount());
-  Serial.print(F("Blocks x Cluster:  "));
-  Serial.println(volume.blocksPerCluster());
-
-  Serial.print(F("Total Blocks:      "));
-  Serial.println(volume.blocksPerCluster() * volume.clusterCount());
-  Serial.println();
-
-  // print the type and size of the first FAT-type volume
-  uint32_t volumesize;
-  Serial.print(F("Volume type is:    FAT"));
-  Serial.println(volume.fatType(), DEC);
-
-  volumesize = volume.blocksPerCluster();    // clusters are collections of blocks
-  volumesize *= volume.clusterCount();       // we'll have a lot of clusters
-  volumesize /= 2;                           // SD card blocks are always 512 bytes (2 blocks are 1KB)
-  Serial.print("Volume size (Kb):  ");
-  Serial.println(volumesize);
-  Serial.print("Volume size (Mb):  ");
-  volumesize /= 1024;
-  Serial.println(volumesize);
-  Serial.print("Volume size (Gb):  ");
-  Serial.println((float)volumesize / 1024.0);
-
-  Serial.println("\nFiles found on the card (name, date and size in bytes): ");
-  root.openRoot(volume);
-
-  // list all files in the card with date and size
-  root.ls(LS_R | LS_DATE | LS_SIZE);
-
 //  RtcDateTime CurrentTime = RtcDateTime(__DATE__,__TIME__);
 //  Clock.SetDateTime(CurrentTime);
 
 // print the contents of EEPROM  to serial output
-  Serial.println(" contents of EEProm");
-
-  for(int addr = 0; addr < 4096; addr=addr+8){
-    Serial.print(addr);
-    for(int a=0; a<8; a++){
-      Serial.print("\t");
-      Serial.print(EEPROM.read(addr + a));
-    }
-   Serial.println();
-  }
+//  Serial.println(" contents of EEProm");
+//
+//  for(int addr = 0; addr < 4096; addr=addr+8){
+//    Serial.print(addr);
+//    for(int a=0; a<8; a++){
+//      Serial.print("\t");
+//      Serial.print(EEPROM.read(addr + a));
+//    }
+//   Serial.println();
+//  }
 
 } // End of Setup******************************************************************************************************************
 
@@ -518,6 +636,7 @@ void loop() {
   if(userInterface){
     UI();
   }
+
   
   RtcDateTime now = Clock.GetDateTime();
   String timestamp = date2string(now);
@@ -525,8 +644,20 @@ void loop() {
   lcd.clear();
   lcd.print(timestamp);
   printDateTime(now);
-  RFID();
+  
+//  Serial.println("playing");
+//  speak.play("test2.wav");
+//  while(speak.isPlaying()){
+    //RFID();
+    if(!speak.isPlaying()){
+      digitalWrite(SOUND, LOW);
+    }
   delay(2000);
+//  RFID();
+
+
+//  RFID();
+//  delay(2000);
   
   //speak.play("a.wav");
   
@@ -539,20 +670,38 @@ void loop() {
 
 
 
+//String date2string(const RtcDateTime& dateTime){
+//  char datestring[25];
+//
+//  snprintf_P(datestring, (sizeof(datestring) / sizeof(datestring[0])),
+//  PSTR("%02u/%02u/%04u %02u:%02u"),
+//      //("%02u/%02u/%04u %02u:%02u:%02u")
+//      dateTime.Month(),
+//      dateTime.Day(),
+//      dateTime.Year(),
+//      dateTime.Hour(),
+//      dateTime.Minute()//,
+//      //dateTime.Second()
+//      );
+//  return datestring; // I do not know if returning a Char[] instead of a string works, maybe i should null terminate it? ah we're leaving this code anyway
+//}
+
+
+
 String date2string(const RtcDateTime& dateTime){
   char datestring[25];
 
   snprintf_P(datestring, (sizeof(datestring) / sizeof(datestring[0])),
-  PSTR("%02u/%02u/%04u %02u:%02u"),
-      //("%02u/%02u/%04u %02u:%02u:%02u")
+  PSTR("%04u/%02u/%02u %02u:%02u"),
+      //("%04u/%02u/%02u %02u:%02u:%02u")
+      dateTime.Year(),
       dateTime.Month(),
       dateTime.Day(),
-      dateTime.Year(),
       dateTime.Hour(),
       dateTime.Minute()//,
       //dateTime.Second()
       );
-  return datestring; // I do not know if returning a Char[] instead of a string works, maybe i should null terminate it?
+  return datestring; // I do not know if returning a Char[] instead of a string works, maybe i should null terminate it? ah we're leaving this code anyway
 }
 
 #define countof(a) (sizeof(a) / sizeof(a[0]))
@@ -563,7 +712,6 @@ void printDateTime(const RtcDateTime& dt)
   lcd.clear();
   lcd.setCursor(1,1);
   lcd.print(datetime);
-
-    Serial.println(datetime);
+  Serial.println(datetime);
     
 }
